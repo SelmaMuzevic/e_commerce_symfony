06a606e12f9749b7d9afd1a5d46487041ef0eecb
selma <selma@gmail.com>
2017-11-02 11:09:12 +0100

    rajout des valeurs dans database et faire les lien entre les entités

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -6,20 +6,61 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Article;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
 class FrontController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function createArticleAction(Request $request)
     {
         // ici je mettrai les routes pour aller chercher la liste des produits(categories)
+        
+        $category = new Category();
+        $category->setName('Vetements');
 
-        // replace this example code with whatever you need
-        return $this->render('homepage/index.html.twig', [
-            
-        ]);
+        $article = new Article();
+        $article->setNom('Pantalons');
+        $article->setPrix(19.99);
+        $article->setPhoto();
+        $article->setDescription('Ergonomic and stylish!');
+
+        // relate this product to the category
+        $article->setCategory($category);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->persist($article);
+        $em->flush();
+
+        return new Response(
+            'Saved new article with id: '.$article->getId()
+            .'and new category with id: '.$category->getId()
+        );
     }
 
-    
+    public function showAction($productId)
+{
+        $article = $this->getDoctrine()
+        ->getRepository(Article::class)
+        ->findOneByIdJoinedToCategory($articleId);
+
+        $category = $article->getCategory();
+
+}
+
+public function showArticleAction($categoryId)
+{
+     $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->find($categoryId);
+
+    $articles = $category->getArticles();
+
+     }
 }
--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+
 
 /**
  * Article
@@ -10,36 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="article")
  * @ORM\Entity
  */
-class Article
-{
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
-     */
-    private $nom;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="prix", type="integer", nullable=false)
-     */
-    private $prix;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="photo", type="string", length=255, nullable=false)
-     */
-    private $photo;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description", type="string", length=255, nullable=false)
-     */
-    private $description;
-
+class Article {
+     
     /**
      * @var integer
      *
@@ -49,6 +24,178 @@ class Article
      */
     private $id;
 
+    /**
+    * @ORM\ManyToOne(targetEntity="Category", inversedBy="articles")
+    * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+    */
+    private $category;
+     
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true)
+     */
+    private $nom;
 
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="prix", type="integer", nullable=true)
+     */
+    private $prix;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", nullable=true)
+     */
+    private $photo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=true)
+     */
+    private $description;
+ 
+    /**
+     * Set id
+     *
+     * @return integer
+     */
+    public function setId()
+    {
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+   
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Article
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param integer $prix
+     *
+     * @return Article
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return integer
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     */
+    public function setPhoto()
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return Article
+     */
+    public function setCategory(\AppBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    
 }
 
